9801267f5743dbf2a574d816ef82e8b41eb89ce0
Anartz <anartz.pagaldai@ikasle.egibide.org>
2023-05-09 14:12:02 +0200

    entity manager

--- P/Kings League/src/main/java/Modelo/EntityManager.java
+++ P/Kings League/src/main/java/Modelo/EntityManager.java
@@ -1,2 +1,65 @@
-package Modelo;public class EntityManager {
+package Modelo;
+
+import Modelo.Jornadas.JornadasEntity;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import javax.swing.*;
+
+public class EntityManager {
+    private EntityManagerFactory emf;
+    private jakarta.persistence.EntityManager em;
+    private EntityTransaction transaction;
+
+    public EntityManager() {
+        emf= Persistence.createEntityManagerFactory ("default");
+        em=emf.createEntityManager();
+        transaction=em.getTransaction();
+    }
+    public void insertar(Object objeto) {
+        try {
+            transaction.begin();
+            em.persist(objeto);
+            transaction.commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getClass() + e.getMessage());
+        }
+    }
+
+    public void actualizar(Object objeto) {
+        try {
+            transaction.begin();
+            objeto = em.find(objeto.getClass(),objeto);
+            if (objeto != null) {
+                em.persist(objeto);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getClass() + e.getMessage());
+        }
+    }
+
+    public void eliminar(Object objeto) {
+        try {
+            transaction.begin();
+            objeto = em.find(objeto.getClass(),objeto);
+            if (objeto != null) {
+                em.remove(objeto);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getClass() + e.getMessage());
+        }
+    }
+
+    public void consultar(Object objeto) {
+        try {
+            transaction.begin();
+            objeto = em.find(objeto.getClass(),objeto);
+            transaction.commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getClass() + e.getMessage());
+        }
+    }
 }
--- P/Kings League/src/main/java/Modelo/Jornadas/JornadasEntityManager.java
+++ P/Kings League/src/main/java/Modelo/Jornadas/JornadasEntityManager.java
@@ -1,2 +1,13 @@
-package Modelo.Jornadas;public class JornadasEntityManager {
+package Modelo.Jornadas;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import javax.swing.*;
+
+public class JornadasEntityManager extends Modelo.EntityManager {
+
+
 }
