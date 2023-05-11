7568e3058812b2cd59bfb8cb5f360c96dcb4b440
OiherOleaga <126677251+OiherOleaga@users.noreply.github.com>
2023-05-11 08:52:18 +0200

    Add files via upload

---
+++ BD/CREATES/llamarPaquetes.sql
@@ -0,0 +1,4 @@
+@/kingsLeague/BD/paquetes/PAQUETE_CALENDARIO_CABECERA.SQL;
+@/kingsLeague/BD/paquetes/PAQUETE_CALENDARIO_BODY.SQL;
+@/kingsLeague/BD/paquetes/PAQUETE_INFORME_CABECERA.SQL;
+@/kingsLeague/BD/paquetes/PAQUETE_INFORME_BODY.SQL;
