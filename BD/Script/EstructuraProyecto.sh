4c598ff0b1ae73aa0cc7e2ddbd5cfb8a9f1a6752
OiherOleaga <126677251+OiherOleaga@users.noreply.github.com>
2023-05-11 09:01:31 +0200

    Add files via upload

---
+++ BD/Script/EstructuraProyecto.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+if [ -d "/kingsLeague" ]; then
+    echo "El directorio /kingsLeague existe."
+else
+    echo "El directorio /kingsLeague no existe."
+    mkdir /kingsLeague
+    echo "Se ha creado el directorio /kingsLeague."
+fi
+
+if [ -d "/kingsLeague/PROG" ]; then
+    echo "El directorio /kingsLeague/PROG existe."
+else
+    echo "El directorio /kingsLeague/PROG no existe."
+    mkdir /kingsLeague/PROG
+    echo "Se ha creado el directorio /kingsLeague/PROG."
+fi
+
+if [ -d "/kingsLeague/BD" ]; then
+    echo "El directorio /kingsLeague/BD existe."
+else
+    echo "El directorio /kingsLeague/BD no existe."
+    mkdir /kingsLeague/BD
+    echo "Se ha creado el directorio /kingsLeague/BD."
+fi
+
+if [ -d "/kingsLeague/ED" ]; then
+    echo "El directorio /kingsLeague/ED existe."
+else
+    echo "El directorio /kingsLeague/ED no existe."
+    mkdir /kingsLeague/ED
+    echo "Se ha creado el directorio /kingsLeague/ED."
+fi
+
+
+if [ -d "/kingsLeague/BD/creates" ]; then
+    echo "El directorio /kingsLeague/BD/creates existe."
+else
+    echo "El directorio /kingsLeague/BD/creates no existe."
+    mkdir /kingsLeague/BD/creates
+    echo "Se ha creado el directorio /kingsLeague/BD/creates."
+fi
+
+if [ -d "/kingsLeague/BD/inserts" ]; then
+    echo "El directorio /kingsLeague/BD/inserts existe."
+else
+    echo "El directorio /kingsLeague/BD/inserts no existe."
+    mkdir /kingsLeague/BD/inserts
+    echo "Se ha creado el directorio /kingsLeague/BD/inserts."
+fi
+
+if [ -d "/kingsLeague/BD/triggers" ]; then
+    echo "El directorio /kingsLeague/BD/triggers existe."
+else
+    echo "El directorio /kingsLeague/BD/triggers no existe."
+    mkdir /kingsLeague/BD/triggers
+    echo "Se ha creado el directorio /kingsLeague/BD/triggers."
+fi
+
+if [ -d "/kingsLeague/BD/MERyrelacional" ]; then
+    echo "El directorio /kingsLeague/BD/MERyrelacional existe."
+else
+    echo "El directorio /kingsLeague/BD/MERyrelacional no existe."
+    mkdir /kingsLeague/BD/MERyrelacional
+    echo "Se ha creado el directorio /kingsLeague/BD/MERyrelacional."
+fi
+
+if [ -d "/kingsLeague/BD/MERyrelacional" ]; then
+    echo "El directorio /kingsLeague/BD/MERyrelacional existe."
+else
+    echo "El directorio /kingsLeague/BD/MERyrelacional no existe."
+    mkdir /kingsLeague/BD/MERyrelacional
+    echo "Se ha creado el directorio /kingsLeague/BD/MERyrelacional."
+fi
+
+if [ -d "/kingsLeague/ED/Diagramas" ]; then
+    echo "El directorio /kingsLeague/ED/Diagramas existe."
+else
+    echo "El directorio /kingsLeague/ED/Diagramas no existe."
+    mkdir /kingsLeague/ED/Diagramas
+    echo "Se ha creado el directorio /kingsLeague/ED/Diagramas."
+fi
+
+if [ -d "/kingsLeague/lmsg/Clasificacion" ]; then
+    echo "El directorio /kingsLeague/lmsg/Clasificacion existe."
+else
+    echo "El directorio /kingsLeague/lmsg/Clasificacion no existe."
+    mkdir /kingsLeague/lmsg/Clasificacion
+    echo "Se ha creado el directorio /kingsLeague/lmsg/Clasificacion."
+fi
+
+if [ -d "/kingsLeague/lmsg/Jornadas" ]; then
+    echo "El directorio /kingsLeague/lmsg/Jornadas existe."
+else
+    echo "El directorio /kingsLeague/lmsg/Jornadas no existe."
+    mkdir /kingsLeague/lmsg/Jornadas
+    echo "Se ha creado el directorio /kingsLeague/lmsg/Jornadas."
+fi
+
+if [ -d "/kingsLeague/PROG/ProgramaJava" ]; then
+    echo "El directorio /kingsLeague/PROG/ProgramaJava existe."
+else
+    echo "El directorio /kingsLeague/PROG/ProgramaJava no existe."
+    mkdir /kingsLeague/PROG/ProgramaJava
+    echo "Se ha creado el directorio /kingsLeague/PROG/ProgramaJava."
+fi
