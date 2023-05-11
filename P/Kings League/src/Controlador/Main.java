26f5f98f4f0bf17f2356dd589c7e3924304c986a
Lorena Arbé Sánchez <lorena.arbe@ikasle.egibide.org>
2023-05-11 09:03:25 +0200

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 93d79d7a6e7c5c67e13ddeadceeeaeb8814ea73a.

--- P/Kings League/src/Controlador/Main.java
+++ P/Kings League/src/Controlador/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Controlador;
 
 import Vista.vInicioSesion;
@@ -31,38 +30,4 @@ public class Main {
         frame.setVisible(true);
         frame.setExtendedState(Frame.MAXIMIZED_BOTH);
     }
-=======
-package Controlador;
-
-import Vista.vInicioSesion;
-import Vista.vPrincipal;
-
-import javax.swing.*;
-import java.awt.*;
-import java.net.MalformedURLException;
-
-public class Main {
-    public static void main(String[] args) throws MalformedURLException {
-        generarVentanaInicio();
-    }
-
-
-    public static void generarVentanaInicio () throws MalformedURLException {
-        JFrame frame = new JFrame("vInicioSesion");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        vInicioSesion inicioSesion = new vInicioSesion();
-        frame.setContentPane(inicioSesion.getpPrincipal());
-        frame.pack();
-        frame.setVisible(true);
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-    }
-    public static void generarVentanaPrincipal () throws MalformedURLException {
-        JFrame frame = new JFrame("vPrincipal");
-        frame.setContentPane(new vPrincipal().getpPrincipal());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-    }
->>>>>>> main
 }
