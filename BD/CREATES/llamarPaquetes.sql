981f9e7b6ebac9bdd053b8025583dd476dfac811
Anartz <126472304+AnartzPagaldai@users.noreply.github.com>
2023-05-17 11:01:51 +0200

    Add files via upload

--- BD/CREATES/llamarPaquetes.sql
+++ BD/CREATES/llamarPaquetes.sql
@@ -1,4 +1,3 @@
 @/Reto-colaborativo-Grupo01-main/BD/CREATES/paquetes/PAQUETE_CALENDARIO_CABECERA.SQL;
 @/Reto-colaborativo-Grupo01-main/BD/CREATES/paquetes/PAQUETE_CALENDARIO_BODY.SQL;
-@/Reto-colaborativo-Grupo01-main/BD/CREATES/paquetes/PAQUETE_INFORME_CABECERA.SQL;
-@/Reto-colaborativo-Grupo01-main/BD/CREATES/paquetes/PAQUETE_INFORME_BODY.SQL;
+@/Reto-colaborativo-Grupo01-main/BD/CREATES/paquetes/INFORMES.SQL;
