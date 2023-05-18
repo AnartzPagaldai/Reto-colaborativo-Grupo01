50d8272a92b7efb6b01d9d7f753ee1f5b9022175
Anartz <anartz.pagaldai@ikasle.egibide.org>
2023-05-18 10:51:30 +0200

    getUltima jornada

--- BD/CREATES/paquetes/PAQUETE_XML_BODY.sql
+++ BD/CREATES/paquetes/PAQUETE_XML_BODY.sql
@@ -1,7 +1,7 @@
-CREATE OR REPLACE PACKAGE BODY PAQUETE_XML AS    
+CREATE OR REPLACE PACKAGE BODY PAQUETE_XML AS
     PROCEDURE GENERAR_XML_JORNADAS AS
         RESULT CLOB;
-        C_PARTIDOS INFORMES.TCURSOR;
+        C_PARTIDOS SYS_REFCURSOR;
         NUMERO_JORNADA_ANTERIOR NUMBER := 0;
         TIPO_SPLIT VARCHAR2(10);
         NUMERO_JORNADA NUMBER(3);
@@ -55,13 +55,13 @@ CREATE OR REPLACE PACKAGE BODY PAQUETE_XML AS
     PROCEDURE GENERAR_XML_CLASIFICACION AS
         RESULT CLOB;
     BEGIN
-        RESULT := '<?xml version="1.0" encoding="UTF-8"?> 
+        RESULT := '<?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE clasificacion SYSTEM "clasificacion.dtd">
         <clasificacion split="1" xmlns:xs="http://www.w3.org/2001/XMLSchema-instance" xs:noNamespaceSchemaLocation="clasificacion.xsd">
-        <fecha>' || TO_DATE(SYSDATE, 'DD/MM/YYYY') || '</fecha>'
+        <fecha>' || TO_DATE(SYSDATE, 'DD/MM/YYYY') || '</fecha>';
         FOR FILA IN (SELECT * FROM CLASIFICACION) LOOP
-                RESULT :=  RESULT ||      
-            '<equipo posicion="' || FILA.POSICION || '">
+                RESULT :=  RESULT ||
+                           '<equipo posicion="' || FILA.POSICION || '">
                 <nombre>' || FILA.EQUIPO || '</nombre>
                 <victorias>' || FILA.VICTORIAS || '</victorias>
                 <goles_a_favor>' || FILA.GOLES_A_FAVOR || '</goles_a_favor>
@@ -77,27 +77,27 @@ CREATE OR REPLACE PACKAGE BODY PAQUETE_XML AS
     PROCEDURE GENERAR_XML_ULTIMA_JORNADA (XML OUT CLOB) AS
         CURSOR C_XML IS
             SELECT S.TIPO TIPO_SPLIT,
-            J.NUM_JORNADA NUMERO_JORNADA,
-            J.TIPO TIPO_JORNADA,
-            P.*,
-            (SELECT NOMBRE FROM EQUIPOS WHERE ID = ID_EQUIPO1) EQUIPO1,
-            (SELECT NOMBRE FROM EQUIPOS WHERE ID = ID_EQUIPO2) EQUIPO2
-        FROM SPLIT S, JORNADAS J, PARTIDOS P
-        WHERE S.ID=J.ID_SPLIT AND J.ID=P.ID_JORNADA
-        AND S.ID=(SELECT MAX(ID) FROM SPLIT) AND J.ID = (SELECT MAX(JORNADAS.ID) FROM JORNADAS, PARTIDOS WHERE FECHA < SYSDATE AND PARTIDOS.ID_JORNADA = JORNADAS.ID);
+                   J.NUM_JORNADA NUMERO_JORNADA,
+                   J.TIPO TIPO_JORNADA,
+                   P.*,
+                   (SELECT NOMBRE FROM EQUIPOS WHERE ID = ID_EQUIPO1) EQUIPO1,
+                   (SELECT NOMBRE FROM EQUIPOS WHERE ID = ID_EQUIPO2) EQUIPO2
+            FROM SPLIT S, JORNADAS J, PARTIDOS P
+            WHERE S.ID=J.ID_SPLIT AND J.ID=P.ID_JORNADA
+              AND S.ID=(SELECT MAX(ID) FROM SPLIT) AND J.ID = (SELECT MAX(JORNADAS.ID) FROM JORNADAS, PARTIDOS WHERE FECHA < SYSDATE AND PARTIDOS.ID_JORNADA = JORNADAS.ID);
         JORNADA NUMBER(1) := 0;
     BEGIN
         XML := XML || '<?xml version="1.0" encoding="UTF-8"?>
             <jornadas xmlns:xs = "http://www.w3.org/2001/XMLSchema-instance" xs:noNamespaceSchemaLocation = "jornadas.xsd">';
         FOR FILA IN  C_XML LOOP
-            IF JORNADA = 0 THEN
-                XML := XML || '<jornada num_jornada="' || FILA.NUMERO_JORNADA || '" id_jornada="' || FILA.ID_JORNADA || '">
+                IF JORNADA = 0 THEN
+                    XML := XML || '<jornada num_jornada="' || FILA.NUMERO_JORNADA || '" id_jornada="' || FILA.ID_JORNADA || '">
                     <tipo_split>' || FILA.TIPO_SPLIT || '</tipo_split>
                     <tipo_jornada>' || FILA.TIPO_JORNADA || '</tipo_jornada>';
-                JORNADA := 1;
-            END IF;
-            XML := XML ||
-                      '<partido id_partido="'|| FILA.ID ||'">
+                    JORNADA := 1;
+                END IF;
+                XML := XML ||
+                       '<partido id_partido="'|| FILA.ID ||'">
             <equipo1>'|| FILA.EQUIPO1 || '</equipo1>
             <goles_equipo1>' || FILA.GOLES_EQUIPO1 || '</goles_equipo1>
             <equipo2>'|| FILA.EQUIPO2 || '</equipo2>
@@ -105,8 +105,7 @@ CREATE OR REPLACE PACKAGE BODY PAQUETE_XML AS
             <fecha_partido>' || FILA.FECHA || '</fecha_partido>
             <lugar_partido>' || FILA.LUGAR || '</lugar_partido>
             </partido>';
-        END LOOP;
+            END LOOP;
         XML := XML || '</jornada></jornadas>';
     END GENERAR_XML_ULTIMA_JORNADA;
 END PAQUETE_XML;
-        
