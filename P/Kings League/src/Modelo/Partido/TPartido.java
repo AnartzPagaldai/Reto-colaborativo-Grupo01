90a1562bd5b71833a9a25738e7ac177cf89a4a07
Oiher Oleaga <oiher.oleaga@ikasle.egibide.org>
2023-05-18 12:37:45 +0200

    UpdatePArtidos

--- P/Kings League/src/Modelo/Partido/TPartido.java
+++ P/Kings League/src/Modelo/Partido/TPartido.java
@@ -10,9 +10,9 @@ public class TPartido {
     public static boolean actualizarPartido (Partido partido)
     {
         boolean valido = true;
-        BaseDeDatos.executeUpdate("update partidos set ", );
+        BaseDeDatos.executeUpdate("update partidos set GOLES_EQUIPO1 = , GOLES_EQUIPO2 = where ID_EQUIPO1 = , ID_EQUIPO2 =   ",new Object[]{partido.getGolesEquipo1(), partido.getGolesEquipo2(), partido.getEquipo1().getId(), partido.getEquipo2().getId()});
 
-    return valido}
+    return valido;}
     public static boolean insertarPartido(Partido partido) {
         return BaseDeDatos.executeUpdate("inset into partidos (id_jornada, fecha, lugar, id_equipo1, id_equipo2) values ((select max(id) from jornadas),?,?,?,?) ",
                 new Object[]{partido.getFecha(), partido.getLugar(), partido.getEquipo1(), partido.getEquipo2()});
